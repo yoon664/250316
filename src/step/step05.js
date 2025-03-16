e7bf1bd3f62c9a8501864afb66bfda53b3fff875
yoon664 <yoonj0264@gmail.com>
2025-03-16 11:58:33 +0900

    Modify step05.js

--- src/step/step05.js
+++ src/step/step05.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Step05 = () => {
     /*
         [ useState ]
         상태값을 저장하는 변수 (중요한 데이터는 변수말고 state에 담아서 사용)
+        상태값이 변경되면 그 상태값을 사용하는 html 부분만 재렌더링
+        (자바스크립트 : 변수를 재할당하면 html 전체 재렌더링)
+
+        const [값, 함수] = useState(초기값);
+        - 함수 : 상태값을 변경하는 함수
     */
+    
+    const [count, setCount] = useState(0); // let [a,b] = [10,20]
+
   return (
     <>
         <div>
-            <p>현재 카운트 : 0</p>
-            <button>카운트증가</button>
+            <p>현재 카운트 : {count}</p>
+            <button onClick={()=>{setCount(count + 1)}}>카운트증가</button>
         </div>
     </>
   )
